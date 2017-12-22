99c67a2eb5a80f4c4903322c96af508c026fc6af
sombriks <sombriks@gmail.com>
2017-12-22 16:05:18 -0300

    the use of zero xml i want to believe

--- gm-sample-worklog-service/src/main/java/br/com/gm/worklog/EntryPoint.java
+++ gm-sample-worklog-service/src/main/java/br/com/gm/worklog/EntryPoint.java
@@ -1,8 +1,32 @@
 package br.com.gm.worklog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gm.worklog.resource.UserResource;
+
+@RestController
+@SpringBootApplication
 public class EntryPoint {
+
+  @Autowired
+  private UserResource user;
+
+
+  @RequestMapping("/user")
+  public UserResource getUser() {
+    return user;
+  }
+
+  @RequestMapping("/status")
+  public String status() {return "ONLINE"; }
 
   public static void main(String ...args) { 
     System.out.println("Alive and kicking!");
+    SpringApplication.run(EntryPoint.class, args); 
+
   }
 }
