2613e67ee5d4b388fd00cc04ba73c05729bd57fe
sombriks <sombriks@gmail.com>
2017-12-26 04:44:56 -0300

    working on jwt thing. we're not going into spring-security thing since it's too wired into springmvc.

--- gm-sample-worklog-service/src/main/java/br/com/gm/worklog/security/SecFilter.java
+++ gm-sample-worklog-service/src/main/java/br/com/gm/worklog/security/SecFilter.java
@@ -4,6 +4,7 @@ import br.com.gm.worklog.business.Users;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -11,6 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+
+import java.security.Key;
 
 // https://stackoverflow.com/a/47846023/420096
 @WebFilter({ "/user", "/worklog" })
@@ -27,12 +34,15 @@ public class SecFilter extends OncePerRequestFilter {
 
     if (true || verbs.contains(request.getMethod())) {
       String header = request.getHeader("Authorization");
-      if(header == null){
+      if (header == null) {
         response.setStatus(401);
         return;
-      }else {
-        
+      } else {
+        Key key = new SecretKeySpec("e1e2e3e4".getBytes(), SignatureAlgorithm.HS256.getJcaName());
+        String userLogin = Jwts.parser().setSigningKey(key).parseClaimsJws(header).getBody().getSubject();
+        System.out.println(userLogin);
       }
+
     }
     filterChain.doFilter(request, response);
   }
